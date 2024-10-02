52e9a26c6876d5e3f5b464f6b42048d80294d74a
Cody Bezard <cody.bezard@gmail.com>
2024-10-02 14:45:57 +0200

    v3:Ajout de variable

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String args []) {
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        long d = 1;
+        float e = 2.2F;
+        double f = 1.0d;
+        char g = 'G';
+        boolean h = false;
+        String i = "Hola";
+        System.out.println(e);
+        String randomString = "Voici le résultat d'un calcul : 1+5=6";
+        System.out.println(randomString);
+        //Ajout d'un saut de ligne sur le randomString//
+        String randomString2 = "Voici le résultat d'un calcul :\n 1+5=6";
+        System.out.println(randomString2);
+
+    }
+}
